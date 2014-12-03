cf1999c7828f58e27568fc49fc520a2a7ce1a862
Joakim Tosteberg <joakim.tosteberg@gmail.com>
2014-12-03 13:53:29 +0000

    Added simple testprogram for ADC. Not sure if we got it working yet

---
+++ src/ADS1115_test.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <sstream>
+#include "ADS1115.h"
+
+int main(int argc, const char** argv)
+{
+  int addr = 0;
+  int channel = 0;
+  int continuous = (argc == 4);
+
+  if(argc == 3 || argc == 4) {
+    std::istringstream(argv[1]) >> std::hex >> addr;
+    std::istringstream(argv[2]) >>  channel;
+  } else {
+    std::cout << argv[0] << " addr channel <cont>" << std::endl;
+    return 1;
+  }
+
+  ADS1115 adc(addr);
+  adc.initialize();
+
+  switch(channel) {
+  case 0:
+    adc.setMultiplexer(ADS1115_MUX_P0_NG);
+    break;
+  case 1:
+    adc.setMultiplexer(ADS1115_MUX_P1_NG);
+    break;
+  case 2:
+    adc.setMultiplexer(ADS1115_MUX_P2_NG);
+    break;
+  case 3:
+    adc.setMultiplexer(ADS1115_MUX_P3_NG);
+    break;
+  }
+
+  if(continuous) {
+    adc.setMode(ADS1115_MODE_CONTINUOUS);
+  }
+  do {
+    std::cout << "ADC channel " << channel << ": " << adc.getConversion() << " mV" << std::endl;
+    usleep(100*1000);
+  } while(continuous);
+
+  return 0;
+}
