d02b44cf2769a9d16d28a577164f0e0274169a82
Joakim Tosteberg <joakim.tosteberg@gmail.com>
2014-12-05 11:58:38 +0100

    Update emulation layer

--- src/emulation/wiringPiI2C.h
+++ src/emulation/wiringPiI2C.h
@@ -1,5 +1,7 @@
 inline int wiringPiI2CSetup(uint8_t addr) { return 0; }
 inline int wiringPiI2CWriteReg8(int fd, uint8_t reg, uint8_t val) { return 0; }
 inline int wiringPiI2CReadReg8(int fd, uint8_t reg) { return 0; }
+inline int wiringPiI2CReadBlockData (int fd, int reg, int length, uint8_t* values) { return 0; }
 inline int wiringPiI2CWriteReg16(int fd, uint8_t reg, uint16_t val) { return 0; }
 inline int wiringPiI2CReadReg16(int fd, uint8_t reg) { return 0; }
+inline int wiringPiI2CWriteBlockData (int fd, int reg, int length, uint8_t* values) { return 0; }
