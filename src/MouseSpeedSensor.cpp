fadfcdb88d2b6555ed34b49a6c0f044231315c5e
Joakim Tosteberg <joakim.tosteberg@gmail.com>
2014-12-09 11:00:50 +0000

    Some corrections to mouse speed sensor

--- src/MouseSpeedSensor.cpp
+++ src/MouseSpeedSensor.cpp
@@ -16,7 +16,7 @@ MouseSpeedSensor::~MouseSpeedSensor()
 
 bool MouseSpeedSensor::initialize(const char* device)
 {
-  m_Fd = open(device, O_RDWR);
+  m_Fd = open(device, O_RDWR | O_NONBLOCK);
   return (m_Fd != -1);
 }
 
@@ -26,7 +26,19 @@ MouseSpeedSensor::MouseSpeed MouseSpeedSensor::getSpeed()
   uint8_t data[3];
   if(read(m_Fd, data, sizeof(data)) == sizeof(data)) {
     speed.x = data[1];
+    if(data[0] & (1<<6)) {
+      speed.x = 1000;
+    }
+    if(data[0] & (1<<4)) {
+      speed.x = -speed.x;
+    }
     speed.y = data[2];
+    if(data[0] & (1<<7)) {
+      speed.y = 1000;
+    }
+    if(data[0] & (1<<5)) {
+      speed.y = -speed.y;
+    }
   }
   return speed;
 }
