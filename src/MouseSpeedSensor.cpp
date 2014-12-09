76948cbc58ee194becd3b57a8a51c2a05d11935f
Joakim Tosteberg <joakim.tosteberg@gmail.com>
2014-12-09 12:11:05 +0000

    Updated speed sensor again...

--- src/MouseSpeedSensor.cpp
+++ src/MouseSpeedSensor.cpp
@@ -1,5 +1,6 @@
 #include "MouseSpeedSensor.h"
 
+#include <iostream>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -23,22 +24,10 @@ bool MouseSpeedSensor::initialize(const char* device)
 MouseSpeedSensor::MouseSpeed MouseSpeedSensor::getSpeed()
 {
   MouseSpeed speed = {0,0};
-  uint8_t data[3];
+  int8_t data[3];
   if(read(m_Fd, data, sizeof(data)) == sizeof(data)) {
     speed.x = data[1];
-    if(data[0] & (1<<6)) {
-      speed.x = 1000;
-    }
-    if(data[0] & (1<<4)) {
-      speed.x = -speed.x;
-    }
     speed.y = data[2];
-    if(data[0] & (1<<7)) {
-      speed.y = 1000;
-    }
-    if(data[0] & (1<<5)) {
-      speed.y = -speed.y;
-    }
   }
   return speed;
 }
